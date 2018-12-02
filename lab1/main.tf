6a02155c4bcc98040d995b7f69f1d4009740d702
Carlos Salas <salash66@gmail.com>
2018-12-02 21:22:02 +0100

    first terraform in azure commit

---
+++ lab1/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "lab1" {
+  name     = "terraform-lab1"
+  location = "West Europe"
+
+  tags {
+    environment = "training"
+  }
+}
+
+resource "azurerm_storage_account" "lab1sa" {
+  name                     = "isthisaveryuniquenameah"
+  resource_group_name      = "${azurerm_resource_group.lab1.name}"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
---
+++ lab2/variables.tf
@@ -0,0 +1,16 @@
+variable "rg" {
+    default = "terraform-lab2"
+}
+
+variable "loc" {
+    default = "West Europe"
+}
+
+variable "tags" {
+    type = "map"
+    default = {
+        environment = "training"
+        source      = "citadel"
+    }
+}
+
